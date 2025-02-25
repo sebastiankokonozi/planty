327881b1efbc3b4acefbf03c6a2ccde3bc99aae4
Sebastian Kokonozi <105063242+sebastiankokonozi@users.noreply.github.com>
2025-02-25 10:47:23 +0100

    added layout groups , added oboadring

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,36 @@
+import { Redirect, Tabs } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
+import { Feather } from "@expo/vector-icons";
+import { theme } from "@/theme";
+
+const hasFinishedOnboarding = false;
+
+export default function Layout() {
+  if (!hasFinishedOnboarding) {
+    return <Redirect href="/onboarding" />;
+  }
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorGreen }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size, color }) => (
+            <Entypo name="leaf" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarShowLabel: false,
+          title: "Profile",
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="user" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
