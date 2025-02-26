edb05f62be7abe99db37110c0d6dc1f2d8aad496
Sebastian Kokonozi <105063242+sebastiankokonozi@users.noreply.github.com>
2025-02-26 12:27:36 +0100

    Save plants in storage and list on the home page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,14 +3,24 @@ import { Stack } from "expo-router";
 export default function Layout() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen
-        name="onboarding"
+        name="(tabs)"
         options={{ headerShown: false, animation: "fade" }}
       />
       <Stack.Screen
+        name="onboarding"
+        options={{
+          presentation: "modal",
+          headerShown: false,
+          animation: "fade",
+        }}
+      />
+      <Stack.Screen
         name="new"
-        options={{ presentation: "modal", title: "New Plant" }}
+        options={{
+          presentation: "modal",
+          title: "New plant",
+        }}
       />
     </Stack>
   );
